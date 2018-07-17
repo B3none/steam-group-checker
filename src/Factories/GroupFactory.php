92e0f8bd8e87ac7f0a8e55bd6a438c95f1e9cbd2
b3none <ablackham2000@gmail.com>
2018-07-17 23:37:52 +0100

    Added a caching layer into the GroupFactory.php

--- src/Factories/GroupFactory.php
+++ src/Factories/GroupFactory.php
@@ -2,17 +2,12 @@
 
 namespace B3none\SteamGroupChecker\Factories;
 
+use B3none\Cache\CacheClient;
 use B3none\SteamGroupChecker\Models\Group;
-use GuzzleHttp\Client;
 
 class GroupFactory
 {
     const GROUP_XML = "/memberslistxml/?xml=1";
-
-    /**
-     * @var Client
-     */
-    protected $client;
 
     /**
      * @var Group
@@ -25,20 +20,39 @@ class GroupFactory
     protected $xpath;
 
     /**
+     * @var CacheClient
+     */
+    protected $cache;
+
+    public function __construct(CacheClient $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    /**
      * @param string $groupUrl
      * @return Group
      */
-    public function processGroup(string $groupUrl)
+    public function processGroup(string $groupUrl) : Group
     {
-        $this->group = new Group();
+        $cacheId = preg_replace("/[^A-Za-z0-9 ]/", '', $groupUrl);
+        if ($this->cache->hasCache($cacheId) && $this->cache->isFreshEnough($cacheId)) {
+            return $this->cache->getCache($cacheId)['group'];
+        } else {
+            $this->group = new Group();
 
-        $this->group->setUrl($groupUrl);
-        $this->getXPath($this->group->getUrl());
-        $this->detectName();
-        $this->detectGroupID();
-        $this->detectMembers();
+            $this->group->setUrl($groupUrl);
+            $this->getXPath($this->group->getUrl());
+            $this->detectName();
+            $this->detectGroupID();
+            $this->detectMembers();
 
-        return $this->group;
+            $this->cache->setCache($cacheId, [
+                'group' => $this->group
+            ]);
+
+            return $this->group;
+        }
     }
 
     /**
@@ -59,6 +73,7 @@ class GroupFactory
 
         for ($pageCount = 1; $pageCount <= $pages; $pageCount++) {
             $members = $this->xpath->xpath("/*/members/steamID64");
+
             for ($memberCount = 0; $memberCount < count($members); $memberCount++) {
                 if (!is_null($members[$memberCount])) {
                     $memberSteamId = $members[$memberCount]->__toString();
