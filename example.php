52254dd494bbb16a7ac1f1c174ec9375358b7a74
Hiraku <hiraku@legionofsensei.de>
2021-07-30 22:18:20 +0200

    Changed blacklist example
    The suggested group no longer exists, which leads to many errors when debugging for the first time and also led me astray at first.

--- example.php
+++ example.php
@@ -7,7 +7,7 @@ $client = \B3none\SteamGroupChecker\Client::create();
 $response = $client->detect("76561198028510846", [
     "https://steamcommunity.com/groups/voidrealitygaming",
 ], [
-    "https://steamcommunity.com/groups/irrel",
+    "https://steamcommunity.com/groups/hentaii",
 ]);
 
 print_r([
